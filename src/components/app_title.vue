3670a349b3bba224e683a7eb4597700feeb70624
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-06-19 16:40:09 +0200

    Fine esercizio fatto bonus

---
+++ src/components/app_title.vue
@@ -0,0 +1,20 @@
+<script>
+    export default {
+     
+    }
+</script>
+<template>
+    <div>
+        <h1>La mia prima app con Vite!</h1>
+    </div>
+</template>
+<style>
+    h1{
+        position: absolute;
+        z-index: 100;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: #fff;
+    }
+</style>
---
+++ src/components/apphero.vue
@@ -0,0 +1,22 @@
+<script>
+    export default {
+     
+    }
+</script>
+<template>
+    <div>
+        <img src="https://picsum.photos/600">
+    </div>
+</template>
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;  
+    }
+    img{
+        position: relative;
+        width: 100%;
+        height: 100vh;
+    }
+</style>
